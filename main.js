43c1455859f6170889abffc00abfe9121dc34778
TamjidBhai <tamtop2000@gmail.com>
2025-11-12 18:57:23 +0600

    changes

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ async function changeDevice(deviceId) {
       const timeoutId = setTimeout(() => controller.abort(), 15000);
 
       const response = await fetch(
-        "/.netlify/functions/store-energy-data?" +
+        "/.netlify/functions/get-smart-plug-data?" +
           new URLSearchParams({
             deviceId: currentDeviceId,
             startTime: new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString(),
