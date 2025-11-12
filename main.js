afd5230aa9596a90ee093f34a0c3a3163422c367
TamjidBhai <tamtop2000@gmail.com>
2025-11-12 11:00:26 +0600

    changes

--- main.js
+++ main.js
@@ -159,8 +159,6 @@ let lastUpdateTimestamp = Date.now();
 const updateIntervalMs = 5000; // API fetch interval (5 seconds)
 
 // --- CHART INITIALIZATION ---
-Chart.register(window["chartjs-plugin-zoom"]);
-Chart.register(ChartZoom);
 const powerCtx = document.getElementById("powerChart").getContext("2d");
 const powerChart = new Chart(powerCtx, {
   type: "line",
